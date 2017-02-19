c4bb8585219d7ef4e9647ad569100953a0aaaad2
thuthuaungayenyein <ayenyeinthuthuaung07@gmail.com>
2017-02-19 22:48:08 +0900

    Listview

---
+++ app/src/main/java/myapp/test/edu/listview/MainActivity.java
@@ -0,0 +1,36 @@
+package myapp.test.edu.listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lv ;
+
+    String[] txt = {
+            "Bangalore",
+            "Chennai",
+            "Mumbai",
+            "Pune",
+            "Delhi",
+            "Jabalpur",
+            "Indore",
+            "Ranchi",
+            "Hyderabad",
+            "Ahmedabad",
+            "Kolkata"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lv = (ListView)findViewById(R.id.myLV);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, txt);
+        lv.setAdapter(adapter);
+
+    }
+}
